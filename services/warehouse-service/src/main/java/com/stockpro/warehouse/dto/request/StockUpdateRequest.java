55efaf636011b226a75756c530e053569c5feef7
3-shravan <shantanuyadav379@gmail.com>
2026-04-22 01:00:27 +0530

    style(warehouse-service): organise imports

--- services/warehouse-service/src/main/java/com/stockpro/warehouse/dto/request/StockUpdateRequest.java
+++ services/warehouse-service/src/main/java/com/stockpro/warehouse/dto/request/StockUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.stockpro.warehouse.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
